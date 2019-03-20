af878075e49d98ae0afaeecb14cb3c83211c2536
rhirech059 <rhirech059@student.fuhsd.org>
2019-03-20 10:56:01 -0700

    Add Utils file.

---
+++ src/Utils.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Utils {
+
+    public static String readFileAsString(String filepath) {
+        StringBuilder output = new StringBuilder();
+
+        try (Scanner scanner = new Scanner(new File(filepath))) {
+
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                output.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+    }
+
+
+
+}
