0b887c5506136a22bc755933b3c1707f4376c7ba
RayanHirech <rhirech059@student.fuhsd.org>
2019-03-23 22:18:56 -0700

    Create DataPoint class.

--- src/CountryUnemployment.java
+++ src/CountryUnemployment.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+
 public class CountryUnemployment {
     //Use this class to store the data under the name of a country
+
+    private String countryName;
+    private String countryCode;
+    private String indicatorName;
+
+    public ArrayList<DataPoint> data;
     
-    public CountryUnemployment(String name, int inputvalue){}
+    public CountryUnemployment(){
+
+        data = new ArrayList<>();
+    }
+
+    public String getCountryName() {
+        return this.countryName;
+    }
+    public String getCountryCode() {
+        return this.countryCode;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public double getAverageData() {
+        return 0;
+    }
+
 }
---
+++ src/DataPoint.java
@@ -0,0 +1,25 @@
+public class DataPoint {
+
+    private int year;
+    private int data;
+
+    public DataPoint(int year, int data) {
+        this.year = year;
+        this.data = data;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+    public int getData() {
+        return this.data;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setData(int data) {
+        this.data = data;
+    }
+
+}
